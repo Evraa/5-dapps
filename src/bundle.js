5e7e723adc58e12f058f38c811e05145169b2010
Evraa <evramyousef@gmail.com>
2020-03-10 00:29:27 +0200

    Second contract, typing hello world out at the screen

--- src/bundle.js
+++ src/bundle.js
@@ -1,8 +1,30 @@
-var contractABI = [];
-var contractAddress = '0x346915E6946cc9493F3eeFcED7E4ed6AF8d0b76c';
+var contractABI = [
+    {
+      "constant": true,
+      "inputs": [],
+      "name": "hello",
+      "outputs": [
+        {
+          "name": "",
+          "type": "string"
+        }
+      ],
+      "payable": false,
+      "stateMutability": "pure",
+      "type": "function"
+    }
+  ];
+var contractAddress = '0xAe3037d07697B3f8eC5f2f1A2AD5B5bf2f441cEE';
 var web3 = new Web3('http://localhost:9545');
-var simpleSmartContract = new web3.eth.Contract(contractABI, contractAddress);
-console.log(simpleSmartContract);
+var helloWorld = new web3.eth.Contract(contractABI, contractAddress);
+console.log(helloWorld);
+
+document.addEventListener('DOMContentLoaded',()=>{
+    helloWorld.methods.hello().call()
+    .then(result =>{
+        document.getElementById('hello').innerHTML = result;
+    });
+});
 
 web3.eth.getAccounts()
 .then(console.log);
